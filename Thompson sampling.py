9057bb0cc94745db2321f0f4482b8bc571dbcb5c
satagood <68193231+satagood@users.noreply.github.com>
2022-04-06 18:32:51 +0300

    Add files via upload

---
+++ Thompson sampling.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import beta
+
+
+# In[ ]:
+
+
+NUM_TRIALS = 10000
+BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
+
+
+# In[7]:
+
+
+class Bandit:
+  def __init__(self, p):
+    self.p = p
+    self.a = 1
+    self.b = 1
+    self.N = 0 # for information only
+
+  def pull(self):
+    return np.random.random() < self.p
+
+  def sample(self):
+    return np.random.beta(self.a, self.b)
+
+  def update(self, x):
+    self.a += x
+    self.b += 1 - x
+    self.N += 1
+
+
+def plot(bandits, trial):
+  x = np.linspace(0, 1, 200)
+  for b in bandits:
+    y = beta.pdf(x, b.a, b.b)
+    plt.plot(x, y, label=f"real p: {b.p:.4f}, win rate = {b.a - 1}/{b.N}")
+  plt.title(f"Bandit distributions after {trial} trials")
+  plt.legend()
+  plt.show()
+
+
+def experiment():
+  bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
+
+  sample_points = [3, 5,10,20,50,100,200,500,1000,1500, 9999]
+  rewards = np.zeros(NUM_TRIALS)
+  for i in range(NUM_TRIALS):
+    # Thompson sampling
+    j = np.argmax([b.sample() for b in bandits])
+
+    # plot the posteriors
+    if i in sample_points:
+      plot(bandits, i)
+
+    # pull the arm for the bandit with the largest sample
+    x = bandits[j].pull()
+
+    # update rewards
+    rewards[i] = x
+
+    # update the distribution for the bandit whose arm we just pulled
+    bandits[j].update(x)
+
+  # print total reward
+  print("total reward earned:", rewards.sum())
+  print("overall win rate:", rewards.sum() / NUM_TRIALS)
+  print("num times selected each bandit:", [b.N for b in bandits])
+
+
+if __name__ == "__main__":
+  experiment()
+
+
+# In[ ]:
+
+
+
+
